c43495ff9a2cdcced40ff9a7b2c0c13a93789eb5
nobody <nobody@no.where>
2025-04-15 00:35:19 +0800

    add missing files

--- src/defines.ts
+++ src/defines.ts
@@ -1,7 +1,7 @@
 export const USE_WS = true;
 export const DBG = {
-  LOG_convertSymbol: true,
-  LOG_WS_ONMESSAGE: true,
-  LOG_WS_SENDMESSAGE: true,
-  LOG_ASSETMAP : true
+  LOG_convertSymbol: false,
+  LOG_WS_ONMESSAGE: false,
+  LOG_WS_SENDMESSAGE: false,
+  LOG_ASSETMAP : false
 }
