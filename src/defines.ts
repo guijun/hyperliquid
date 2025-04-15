42f92ba85aa26a9f8f46bf35e8c4ae71160bd544
nobody <nobody@no.where>
2025-04-15 10:58:48 +0800

    saving

--- src/defines.ts
+++ src/defines.ts
@@ -2,6 +2,6 @@ export const USE_WS = true;
 export const DBG = {
   LOG_convertSymbol: false,
   LOG_WS_ONMESSAGE: false,
-  LOG_WS_SENDMESSAGE: false,
+  LOG_WS_SENDMESSAGE: true,
   LOG_ASSETMAP : false
 }
